5a48418f290dc4ef15495327d5b9b16c9e951420
Justin Quimby <justinquimby@gmail.com>
2016-04-30 12:26:57 -0700

    adding generic set and get functions

--- scripts/blockSpawner.js
+++ scripts/blockSpawner.js
@@ -8,14 +8,63 @@
 
     var targetScript = Script.resolvePath("./clickTest.js");
 
+    //
+    // These are generic functions for setting data on an object.
+    //
+
+    // FIXME fetch from a subkey of user data to support non-destructive modifications
+    function setEntityUserData(id, data) {
+        var json = JSON.stringify(data)
+        Entities.editEntity(id, { userData: json });
+    };
+
+    // FIXME do non-destructive modification of the existing user data
+    function getEntityUserData(id) {
+        var results = null;
+        var properties = Entities.getEntityProperties(id, "userData");
+        if (properties.userData) {
+            try {
+                results = JSON.parse(properties.userData);
+            } catch(err) {
+                print("err");
+                print("properties.userData");
+            }
+        }
+        return results ? results : {};
+    };
+
+    function setEntityUserDataEntry(id, name, value) {
+        var data = {};
+        data[name] = value;
+        var json = JSON.stringify(data)
+        Entities.editEntity(id, { userData: json });
+    };
+
+    function getEntityUserDataEntry(id, name, defaultVal) {
+        var results = getEntityUserData(id);
+        print("getEntityUserData - " + name);
+
+        if (name in results) {
+            // print("found val - " + results[name]);
+            return results[name];
+        }
+
+        print("using default val");
+
+        return defaultVal;
+    }
+
+
+    //
+    // This is the meat of the object
+    //
+
 
     // the "constructor" for our class. pretty simple, it just sets our _this, so we can access it later.
     var _this;
     Spawner = function() {
         _this = this;
     };
-
-    // This is the meat of the object
 
     Spawner.prototype = {
 
@@ -29,7 +78,13 @@
         clickDownOnEntity: function(entityID, mouseEvent) {
             print("Spawner::got click()");
 
-            this.createBlockWall();
+            if (!this.isActive()) {
+                this.createBlockWall();
+
+                this.setActive(true);
+            } else {
+                print("active");
+            }
         },
 
 
@@ -56,6 +111,7 @@
                 }
             }
 
+            setEntityUserDataEntry(this.entityID, "creationCount", num);
         },
 
         createBlock: function(num, widthOffset, heightOffset) {
@@ -79,6 +135,33 @@
 
             return blockID;
         },
+
+        setActive: function(active) {
+            // var data = {};
+            // data["active"] = active;
+            // setEntityUserData(this.entityID, data);
+
+            print("active state: " + active);
+
+            setEntityUserDataEntry(this.entityID, "active", true);
+        },
+
+        isActive: function() {
+            print("isActive");
+            var ret = getEntityUserDataEntry(this.entityID, "active", false);
+            print(ret);
+            return ret;
+            
+            // var results = getEntityUserData(this.entityID);
+
+            // if ("active" in results) {
+            //     print(" found active");
+            //     return results.active;
+            // }
+
+            // return false;
+        },
+
     };
 
     return new Spawner();
