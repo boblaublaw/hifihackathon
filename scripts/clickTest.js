eae17d3a72939e9d1543e7bd92a67e07a47369d4
Joe Boyle <github@sublimation.org>
2016-04-29 21:08:02 -0700

    click script

--- scripts/clickTest.js
+++ scripts/clickTest.js
@@ -1,1 +1,13 @@
-this is a test
+//  changeColorOnClickDown.js
+(function(){ 
+    var clicked = false;
+    this.clickDownOnEntity = function(entityID, mouseEvent) { 
+        if (clicked){
+            Entities.editEntity(entityID, { color: { red: 0, green: 255, blue: 255} });
+            clicked = false;
+        }else{
+            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 0} });
+            clicked = true; 
+        }
+    }; 
+})
