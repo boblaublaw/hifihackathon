603f8725f16fb01c943c24ddf5e2747170a3e508
cwright <cdotwright@gmail.com>
2016-05-01 10:18:31 -0700

    color/params

--- scripts/clickshader.js
+++ scripts/clickshader.js
@@ -3,17 +3,17 @@
     var clicked = false;
     this.clickDownOnEntity = function(entityID, mouseEvent) { 
 
-	var blob = {"ProceduralEntity":{"version":2,"shaderUrl":"https://raw.githubusercontent.com/boblaublaw/hifihackathon/master/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":10,"iWidth":0.081,"grid":51}}};
+	var blob = {"ProceduralEntity":{"version":2,"shaderUrl":"https://raw.githubusercontent.com/boblaublaw/hifihackathon/master/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":10,"iWidth":0.081,"grid":1}}};
 	var blob2 = {"ProceduralEntity":{"version":2,"shaderUrl":"https://raw.githubusercontent.com/boblaublaw/hifihackathon/master/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":1,"iWidth":0.082}}};
 
 	if (clicked){
 	    
             Entities.editEntity(entityID, { userData: (JSON.stringify(blob)) });
-	    Entities.editEntity(entityID, { color: { red: 0, green: 250 , blue: 250} });
+	    Entities.editEntity(entityID, { color: { red: 0, green: 30 , blue: 250} });
             clicked = false;
         }else{
             Entities.editEntity(entityID, { userData: (JSON.stringify(blob2)) });
-            Entities.editEntity(entityID, { color: { red: 255, green: 255, blue: 255} });
+            Entities.editEntity(entityID, { color: { red: 0, green: 5, blue: 255} });
             clicked = true; 
         }
     }; 
