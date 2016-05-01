6a0d1aaaa8a942a4addf706370f9237027c45b45
cwright <cdotwright@gmail.com>
2016-05-01 13:52:08 -0700

    alksjdfhajsdkf

--- scripts/cyberfloor.js
+++ scripts/cyberfloor.js
@@ -3,7 +3,7 @@
 
     var _this;
     var clicked = false;
-	var blob = {"ProceduralEntity":{"version":2,"shaderUrl":"https://raw.githubusercontent.com/boblaublaw/hifihackathon/master/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":10,"iWidth":0.4,"grid":.5}}};
+	var blob = {"ProceduralEntity":{"version":2,"shaderUrl":"https://raw.githubusercontent.com/boblaublaw/hifihackathon/master/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":10,"iWidth":0.04,"grid":5}}};
 
     CyberFloor = function() {
 	_this = this;
