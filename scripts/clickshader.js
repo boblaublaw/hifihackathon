7fd8c48d88d5c49a8f8d27841aa868abb09700fa
cwright <cdotwright@gmail.com>
2016-05-01 06:05:27 -0700

    pathfix

--- scripts/clickshader.js
+++ scripts/clickshader.js
@@ -3,8 +3,8 @@
     var clicked = false;
     this.clickDownOnEntity = function(entityID, mouseEvent) { 
 
-	var blob = {"ProceduralEntity":{"version":2,"shaderUrl":"c:/Users/colin/Desktop/hifihackathon/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":10,"iWidth":0.081,"grid":7}}};
-	    var blob2 = {"ProceduralEntity":{"version":2,"shaderUrl":"c:/Users/colin/Desktop/hifihackathon/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":1,"iWidth":0.082}}};
+	var blob = {"ProceduralEntity":{"version":2,"shaderUrl":"https://raw.githubusercontent.com/boblaublaw/hifihackathon/master/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":10,"iWidth":0.081,"grid":7}}};
+	    var blob2 = {"ProceduralEntity":{"version":2,"https://raw.githubusercontent.com/boblaublaw/hifihackathon/master/scripts/grid.fs","uniforms":{"iSpeed":1,"iShell":1,"iWidth":0.082}}};
 
 	if (clicked){
 	    
